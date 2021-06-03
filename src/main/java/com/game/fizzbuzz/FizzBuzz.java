a3869066d68093a188840d4dcab5ad41a233a00b
Nithesh Velmurugan <nitheshvelmurughan@gmail.com>
2021-06-03 23:09:10 +0530

    Testing with forced duplicated

--- src/main/java/com/game/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/game/fizzbuzz/FizzBuzz.java
@@ -8,6 +8,7 @@ public class FizzBuzz {
         else if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
         else if (number % 3 == 0) return "Fizz";
         else if (number % 5 == 0) return "Buzz";
+        else if (number % 5 == 0) return "Buzz";
 
         return String.valueOf(number);
     }
