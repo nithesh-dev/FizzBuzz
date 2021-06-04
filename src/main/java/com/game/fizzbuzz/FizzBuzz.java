f6c28f78e19b741a88288f35559e01df317d7dc0
Nithesh Velmurugan <nitheshvelmurughan@gmail.com>
2021-06-04 16:14:15 +0530

    Forceful add of bug in code

--- src/main/java/com/game/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/game/fizzbuzz/FizzBuzz.java
@@ -3,14 +3,16 @@ package com.game.fizzbuzz;
 public class FizzBuzz {
 
     public String play(int number) throws IllegalAccessException {
-
+        int numbr = 5;
+        int numbr = 1;
         if (number == 0) throw new IllegalAccessException("Number 0 is not valid");
         else if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
         else if (number % 3 == 0) return "Fizz";
         else if (number % 5 == 0) return "Buzz";
-        else if (number % 5 == 0) return "Buzz"
 
         return String.valueOf(number);
     }
+
+
 
 }
