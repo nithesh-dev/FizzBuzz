24762e00c696144aef324c07a4359250ebe1da61
Nithesh Velmurugan <nitheshvelmurughan@gmail.com>
2021-06-05 22:28:42 +0530

    Test commit

--- src/main/java/com/game/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/game/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,6 @@ package com.game.fizzbuzz;
 public class FizzBuzz {
 
     public String play(int number) throws IllegalAccessException {
-        int numbr = 5;
         if (number == 0) throw new IllegalAccessException("Number 0 is not valid");
         else if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
         else if (number % 3 == 0) return "Fizz";
